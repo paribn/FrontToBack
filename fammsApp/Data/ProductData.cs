0c3017b3052ae5c365fba8cf39178ce3eac7ce4b
perinov <novruzovaperi2@gmail.com>
2023-11-04 20:26:17 +0400

    created databaseproducts come from the database

--- fammsApp/Data/ProductData.cs
+++ fammsApp/Data/ProductData.cs
@@ -1,25 +1,45 @@
-﻿namespace fammsApp.Data;
-using fammsApp.Entities;
+﻿using fammsApp.Entities;
+using Microsoft.Data.SqlClient;
+using System.Data;
+namespace fammsApp.Data;
 public class ProductData
 {
-    private static List<Products> Products = new()
-    {
-        new(){Id=1,Name="Men's shirt",Price=65,ImagePath="p1.png"},
-        new(){Id=2,Name="Men's shirt",Price=30,ImagePath="p2.png"},
-        new(){Id=3,Name="Women's shirt",Price=44,ImagePath="p3.png"},
-        new(){Id=4,Name="Women's dress",Price=75,ImagePath="p4.png"},
-        new(){Id=5,Name="Men's shirt",Price=35,ImagePath="p5.png"},
-        new(){Id=6,Name="Women's shirt",Price=45,ImagePath="p6.png"},
-        new(){Id=7,Name="Women's shirt",Price=74,ImagePath="p7.png"},
-        new(){Id=8,Name="Men's shirt",Price=11,ImagePath="p8.png"},
-        new(){Id=9,Name="Men's shirt",Price=22,ImagePath="p9.png"},
-        new(){Id=10,Name="Men's shirt",Price=25,ImagePath="p10.png"},
-        new(){Id=11,Name="Men's shirt",Price=12,ImagePath="p11.png"},
-        new(){Id=12,Name="Women's dress",Price=84,ImagePath="p12.png"},
-
-    };
+    private const string SqlConnectionString = "Server=DOCUMENT\\SQLEXPRESS;Database=FammsDB;Trusted_Connection=True;Encrypt=false;Trusted_Connection=True;";
     public static List<Products> ListProducts()
     {
-        return Products;
+        SqlConnection con = null;
+        DataSet ds = null;
+        List<Products> products = new();
+
+        try
+        {
+            con = new SqlConnection(SqlConnectionString);
+            var cmd = new SqlCommand("SELECT * FROM dbo.FamProducts", con);
+            con.Open();
+
+            var da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+
+            ds = new DataSet();
+            da.Fill(ds);
+
+            for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+            {
+
+                var product = new Products();
+                product.Name = ds.Tables[0].Rows[i]["Name"].ToString();
+                product.Price = Convert.ToDecimal(ds.Tables[0].Rows[i]["Price"].ToString());
+
+                product.ImagePath = ds.Tables[0].Rows[i]["ImagePath"].ToString();
+
+                products.Add(product);
+            }
+
+        }
+        finally
+        {
+            con.Close();
+        }
+        return products;
     }
 }
